4584410a81f98ab29ef91d71b8cd19032799f1f4
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-05-03 04:15:29 +0200

    Add project files.

---
+++ TestDataGridPluralsight/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestDataGridPluralsight
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+
+        public ObservableCollection<string> Sexes
+        {
+            get { return (ObservableCollection<string>)GetValue(SexesProperty); }
+            set { SetValue(SexesProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Sexes.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SexesProperty =
+            DependencyProperty.Register("Sexes", typeof(ObservableCollection<string>), typeof(MainWindow), new PropertyMetadata(null));
+
+
+        public ObservableCollection<Person> Persons {get { return (ObservableCollection<Person>)GetValue(PersonsProperty); }set { SetValue(PersonsProperty, value); }}
+        public static readonly DependencyProperty PersonsProperty =
+            DependencyProperty.Register("Persons", typeof(ObservableCollection<Person>), typeof(MainWindow), new PropertyMetadata(null));
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            loadPersons();
+            loadsexes();
+        }
+
+        
+        private void loadPersons()
+        {
+            Persons = new ObservableCollection<Person>() { new Person("uno", "maschio"), new Person("una", "femmina") };
+        }
+
+        private void loadsexes()
+        {
+            Sexes = new ObservableCollection<string>() { "maschio", "femmina","altro","non risponde" };
+        }
+
+    }
+}
---
+++ TestDataGridPluralsight/Person.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestDataGridPluralsight
+{
+    public class Person :ObservableObject
+    {
+        private string _name; public string Name { get => _name; set { Set(() => Name, ref _name, value); }}
+        private string _sex; public string Sex { get => _sex; set { Set(() => Sex, ref _sex, value); }}
+
+        public Person(string name, string sex)
+        {
+            Name = name;
+            Sex = sex;
+        }
+    }
+}
